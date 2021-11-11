ff87d2f78fa5a5dc7029ec9090be261958cad4c6
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-10 21:30:08 -0300

    add create category route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
 const port = process.env.SERVER_PORT
 const project = process.env.PROJECT_NAME
 
-app.get('/', (req, res) => res.send(project))
+app.use(express.json())
+
+app.use('/categories', categoriesRoutes)
 
 app.listen(port, () => {
   return console.log(`${project} run in: http://localhost:${port}`)
