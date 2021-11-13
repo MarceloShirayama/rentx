2afc4251f09269b627dcf79837c5ffd3f0b1d49b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-13 12:41:05 -0300

    add typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'reflect-metadata'
+import './database'
 import swaggerUi from 'swagger-ui-express'
 import './shared/container'
 import { router } from './routes'
