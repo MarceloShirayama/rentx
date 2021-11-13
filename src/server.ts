a8ff6c491dca0a514f32ba05345be875a009d77f
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-13 08:17:22 -0300

    add swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express'
 import 'reflect-metadata'
+import swaggerUi from 'swagger-ui-express'
 import './shared/container'
 import { router } from './routes'
+import swaggerFile from './swagger.json'
 
 const app = express()
 const port = process.env.SERVER_PORT
 const project = process.env.PROJECT_NAME
 
 app.use(express.json())
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router)
 
 app.listen(port, () => {
