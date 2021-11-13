7da251a4f88a5fc027d2861a04f198b0846e6ce5
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-13 08:20:50 -0300

    remove upload file

--- src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -30,7 +30,10 @@ export class ImportCategoryUseCase {
           categories.push({ name, description })
         })
 
-        .on('end', () => resolve(categories))
+        .on('end', () => {
+          fs.promises.unlink(file.path)
+          resolve(categories)
+        })
         .on('error', (error) => reject(error))
     })
   }
