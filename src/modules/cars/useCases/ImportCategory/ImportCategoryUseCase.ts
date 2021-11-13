ca7793376441417f67d7be92e2a6158d8d018aa9
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-12 21:40:31 -0300

    add data stream in file upload

--- src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,42 @@
-import { injectable } from 'tsyringe'
+import csvParse from 'csv-parse'
+import fs from 'fs'
+import { inject, injectable } from 'tsyringe'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
 
+interface IImportCategory {
+  name: string
+  description: string
+}
 @injectable()
 export class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file)
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository
+  ) {}
+
+  loadCategories(file: any): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = []
+      const stream = fs.createReadStream(file.path)
+
+      const parseFile = csvParse()
+
+      stream.pipe(parseFile)
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line
+
+          categories.push({ name, description })
+        })
+
+        .on('end', () => resolve(categories))
+        .on('error', (error) => reject(error))
+    })
+  }
+
+  async execute(file: any): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
