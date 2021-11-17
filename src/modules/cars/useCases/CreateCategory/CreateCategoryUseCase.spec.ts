eb879813bff2f10ee1a2956f6f1fd19b4b2dbe5c
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-17 15:25:13 -0300

    Ensure not be able to create a new category with an already registered name

--- src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../errors/AppError'
 import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory'
 import { CreateCategoryUseCase } from './CreateCategoryUseCase'
 
@@ -31,5 +32,24 @@ describe('Create Category', () => {
     expect(categoryInMemory).toHaveProperty('id')
     expect(categoryInMemory?.name).toEqual(categoryFake.name)
     expect(categoryInMemory?.description).toEqual(categoryFake.description)
+  })
+
+  it('Should not be able to create a new category with an already registered name', async () => {
+    expect(async () => {
+      const categoryFake = {
+        name: 'Category Test',
+        description: 'Category Test description'
+      }
+
+      await createCategoryUseCase.execute({
+        name: categoryFake.name,
+        description: categoryFake.description
+      })
+
+      await createCategoryUseCase.execute({
+        name: categoryFake.name,
+        description: categoryFake.description
+      })
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
