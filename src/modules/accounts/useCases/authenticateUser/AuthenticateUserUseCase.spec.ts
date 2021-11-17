d390f85b1fb43e22b9fd963b2f93d1881d711eb5
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-17 17:38:48 -0300

    Ensure Authenticate User be able to authenticate an user

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,41 @@
+import { CreateUserDTO } from '../../dtos/CreateUserDTO'
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory'
+import { CreateUserUseCase } from '../CreateUser/CreateUserUseCase'
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase'
+
+let usersRepositoryInMemory: UsersRepositoryInMemory
+let authenticateUserUseCase: AuthenticateUserUseCase
+let createUserUseCase: CreateUserUseCase
+
+describe('Authenticate User', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory()
+
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    )
+
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
+  })
+
+  it('Should be able to authenticate an user', async () => {
+    const userFake: CreateUserDTO = {
+      name: 'any_name',
+      password: 'any_password',
+      email: 'any_email@mail.com',
+      driver_license: 'any_driver_license'
+    }
+
+    const passwordFake = userFake.password
+
+    await createUserUseCase.execute(userFake)
+
+    const token = await authenticateUserUseCase.execute({
+      email: userFake.email,
+      password: passwordFake
+    })
+
+    expect(token).toHaveProperty('user')
+    expect(token).toHaveProperty('token')
+  })
+})
