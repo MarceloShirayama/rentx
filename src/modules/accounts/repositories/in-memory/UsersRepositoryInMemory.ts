887d7ee872dc6d7bd26ba6ecf10b208a35c21386
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-17 15:59:45 -0300

    add UsersRepositoryInMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,33 @@
+import { CreateUserDTO } from '../../dtos/CreateUserDTO'
+import { User } from '../../entities/Users'
+import { IUsersRepository } from '../IUsersRepository'
+
+export class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = []
+
+  async create(data: CreateUserDTO): Promise<void> {
+    const user = new User()
+
+    Object.assign(user, { ...data })
+
+    this.users.push(user)
+  }
+
+  async list(): Promise<User[]> {
+    const users = this.users
+
+    return users
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find((user) => user.email === email)
+
+    return user
+  }
+
+  async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find((user) => user.id === id)
+
+    return user
+  }
+}
