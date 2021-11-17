bf2d0d2c522f2c1fce8495d641c307610d1dc4f1
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-17 15:23:00 -0300

    add CategoriesRepositoryInMemory

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,26 @@
+import { Category } from '../../entities/Category'
+import { ICategoriesRepository, ICategoryDTO } from '../ICategoriesRepository'
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = []
+
+  async create({ name, description }: ICategoryDTO): Promise<void> {
+    const category = new Category()
+
+    Object.assign(category, { name, description })
+
+    this.categories.push(category)
+  }
+
+  async list(): Promise<Category[]> {
+    const categories = this.categories
+
+    return categories
+  }
+
+  async findByName(name: string): Promise<Category | undefined> {
+    const category = this.categories.find((category) => (category.name = name))
+
+    return category
+  }
+}
