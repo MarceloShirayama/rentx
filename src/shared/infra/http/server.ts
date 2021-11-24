bd524adacf9c1069f10eba055aa4940106272f7b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-24 10:12:41 -0300

    refactor server, put app in its own file.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,32 +1,7 @@
-import express, { NextFunction, Request, Response } from 'express'
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
-import { connectionDatabase } from '../typeorm'
-import { AppError } from '../errors/AppError'
-import { router } from './routes'
-import '../../container'
-import swaggerFile from '../../../swagger.json'
-;(async function CreateConnection() {
-  await connectionDatabase()
-})()
+import { app } from './app'
 
-const app = express()
 const port = process.env.SERVER_PORT
 const project = process.env.PROJECT_NAME
-
-app.use(express.json())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use(router)
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return res.status(err.statusCode).json({ error: err.message })
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${err.message}`
-  })
-})
 
 app.listen(port, () => {
   return console.log(`${project} run in: http://localhost:${port}`)
