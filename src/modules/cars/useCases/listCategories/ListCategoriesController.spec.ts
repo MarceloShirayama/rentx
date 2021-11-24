e6915d50d1a87324d6165b702d70fae40a0107fc
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-24 19:30:46 -0300

    Ensure List Categories Controller be able to list all categories

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,46 @@
+import { v4 as uuidV4 } from 'uuid'
+import request from 'supertest'
+import { Connection } from 'typeorm'
+import { app } from '../../../../shared/infra/http/app'
+import { connectionDatabase } from '../../../../shared/infra/typeorm'
+
+let connection: Connection
+
+describe('List Categories Controller', () => {
+  jest.setTimeout(30000)
+  beforeAll(async () => {
+    connection = await connectionDatabase()
+    await connection.runMigrations()
+  })
+
+  afterAll(async () => {
+    await connection.dropDatabase()
+    await connection.close
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 10000)) // https://stackoverflow.com/questions/50818367/how-to-fix-err-jest-has-detected-the-following-3-open-handles-potentially-keepin
+  })
+
+  it('Should be able to list all categories', async () => {
+    await connection.query(
+      `INSERT INTO categories(id, name, description, created_at)
+      VALUES($1, $2, $3, $4)
+      `,
+      [`${uuidV4()}`, 'any category name', 'any description category', 'now()']
+    )
+
+    await connection.query(
+      `INSERT INTO categories(id, name, description, created_at)
+      VALUES($1, $2, $3, $4)
+      `,
+      [
+        `${uuidV4()}`,
+        'other category name',
+        'other description category',
+        'now()'
+      ]
+    )
+
+    const response = await request(app).get('/categories')
+
+    expect(response.body.length).toBe(2)
+  })
+})
