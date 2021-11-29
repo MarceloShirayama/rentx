89dbe9b4c6ea64c4e535087d98501d7104c57226
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-29 15:12:19 -0300

    add ListRentalsByUserController and route

--- src/shared/infra/http/routes/rentals.route.ts
+++ src/shared/infra/http/routes/rentals.route.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { CreateRentalController } from '../../../../modules/rentals/usesCases/createRental/CreateRentalController'
 import { DevolutionRentalController } from '../../../../modules/rentals/usesCases/devolutionRental/DevolutionRentalController'
+import { ListRentalsByUserController } from '../../../../modules/rentals/usesCases/listRentalsByUser/ListRentalsByUserController'
 import { ensureAuthenticate } from '../middlewares/ensureAuthenticate'
 
 const rentalsRoutes = Router()
 
 const createRentalController = new CreateRentalController()
 const devolutionRentalController = new DevolutionRentalController()
+const listRentalsByUserController = new ListRentalsByUserController()
 
 rentalsRoutes.post('/', ensureAuthenticate, createRentalController.handle)
 
@@ -14,6 +16,12 @@ rentalsRoutes.post(
   '/devolution/:id',
   ensureAuthenticate,
   devolutionRentalController.handle
+)
+
+rentalsRoutes.get(
+  '/user',
+  ensureAuthenticate,
+  listRentalsByUserController.handle
 )
 
 export { rentalsRoutes }
